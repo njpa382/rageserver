17ea74922903d4ac9919f14d446b6313911e72ac
Larguirucho <njpa382@gmail.com>
2020-05-20 18:40:53 +0200

    Adding smailer and smysql

---
+++ app/server/sMysql.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const mysql = require("mysql");
+const connection =  mysql.createPool({
+	host			:	"localhost",       
+	user			: 	"ragerp",
+	password		: 	"-Abcd1234",
+	database		:	"ragerp",
+});
+
+connection.getConnection(function(e) {
+	if (e) 	{
+		console.log("DATABASE IS NOT WORKING");
+		throw e;
+	}
+	else 	{
+		console.log(`DATABASE IS WORKING`);
+	}
+});
+
+module.exports = connection;
