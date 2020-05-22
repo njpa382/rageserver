6818fee5ee5b456377acbef2e42e06896e9dc3f1
Larguirucho <njpa382@gmail.com>
2020-05-22 13:37:44 +0200

    Hospital - death mech 98

--- app/server/Basic/Weapons/sWeapon.js
+++ app/server/Basic/Weapons/sWeapon.js
@@ -21,7 +21,7 @@ mp.events.addCommand("weapon", (player, fullText, weapon, ammo) => {
 	misc.log.debug("---Pruebas de armas ---");
 	
 	var weapon_hash = mp.players.local.allWeapons; 
-	misc.log.debug("---Pruebas de armas --->" + weapon_hash);
+	misc.log.debug("---Pruebas de armas --->" + JSON.stringify(weapon_hash));
 
  });
 
