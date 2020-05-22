49ff41416fbb69e6c2ebd39feaff4b2940c128bd
Larguirucho <njpa382@gmail.com>
2020-05-22 13:15:04 +0200

    Hospital - death mech 92

--- app/server/Basic/Weapons/sWeapon.js
+++ app/server/Basic/Weapons/sWeapon.js
@@ -2,7 +2,7 @@
 const misc = require('../../sMisc');
 const i18n = require('../../sI18n');
 const weaponSlots = new Set([1993361168,1277010230,932043479,690654591,1459198205,195782970,-438797331,896793492,495159329,-1155528315,-515636489,-871913299,-1352759032,-542958961,1682645887,-859470162,-2125426402,2067210266,-538172856,1783244476,439844898,-24829327,1949306232,-1941230881,-1033554448,320513715,-695165975,-281028447,-686713772,347509793,1769089473,189935548,248801358,386596758,-157212362,436985596,-47957369, 575938238]);
-
+const localPlayer = mp.players.local;
 
 class Weapon {
 	constructor () {
@@ -36,4 +36,12 @@ mp.events.addCommand("weapon", (player, fullText, weapon, ammo) => {
         }
     });
     return weapons;
+}
+
+function getWeaponTypeInSlot (weaponSlot) {
+	return mp.game.invoke('0xEFFED78E9011134D', localPlayer.handle, weaponSlot);
+}
+
+function getAmmoWeapon (weaponhash) {
+	return mp.game.invoke('0x015A522136D7F951', localPlayer.handle, weaponhash);
 }
