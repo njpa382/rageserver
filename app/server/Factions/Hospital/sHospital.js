ad67add1b0aaffe9f06d4dbaa0534a7a70ab6d4e
Larguirucho <njpa382@gmail.com>
2020-05-22 12:23:17 +0200

    Hospital - death mech 2

--- app/server/Factions/Hospital/sHospital.js
+++ app/server/Factions/Hospital/sHospital.js
@@ -14,9 +14,9 @@ class Hospital extends sFaction {
 
 		mp.events.add({
 			"playerDeath" : (player, reason, killer) => {
-				//player.call("cMisc-CallServerEvenWithTimeout", ["sHospital-SpawnAfterDeath", 10000]);
+				player.call("cMisc-CallServerEvenWithTimeout", ["sHospital-SpawnAfterDeath", 10000]);
 				
-				misc.log.debug("Armas actuales: " + JSON.stringify(player.weapons.all));
+				
 				let killername;
 				if (killer) killername = killer.name;
 				misc.log.debug(`${player.name} death! Reason: ${reason}, killer: ${killername}`);
