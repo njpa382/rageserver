135600a581930af3b6c97157ab45dfe6d0edf3e1
Larguirucho <njpa382@gmail.com>
2020-05-23 10:44:12 +0200

    Fix multijob start work 14

--- app/server/Factions/sFactionsJob.js
+++ app/server/Factions/sFactionsJob.js
@@ -186,6 +186,8 @@ class FactionJob extends Job {
         misc.log.debug("player.faction.faction_id: " + player.faction.faction_id);
         misc.log.debug("this.faction_id: " + this.faction_id);
         misc.log.debug("this.className: " + this.className);
+        misc.log.debug("this.name: " + this.name);
+        misc.log.debug("IsfromThisFaction? : " + player.faction.faction_id === this.faction_id);
         
         return player.faction.faction_id === this.faction_id;
     }
