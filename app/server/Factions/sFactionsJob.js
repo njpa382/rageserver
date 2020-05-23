1901dcc9740a3dc439ebae5402dd012403a5e80b
Larguirucho <njpa382@gmail.com>
2020-05-23 09:54:20 +0200

    Fix multijob start work 9

--- app/server/Factions/sFactionsJob.js
+++ app/server/Factions/sFactionsJob.js
@@ -200,7 +200,7 @@ class FactionJob extends Job {
             player.notify(`~r~${i18n.get(this.className, 'younotinthisfaction', this.lang)}!`);
         }
         */
-
+       misc.log.debug("startWork XXXXXXXXXXXXXXXXXXX");
        super.startWork(player);
        player.job = { name: player.faction.name, faction_id: player.faction.faction_id, isActive: true };
        misc.log.debug("this.blipInfo: " + JSON.stringify(player.job));
