3291b1dd20afda766016966d8b70af80744a7ebe
Larguirucho <njpa382@gmail.com>
2020-05-23 09:39:39 +0200

    Fix multijob start work 5

--- app/server/Jobs/sJob.js
+++ app/server/Jobs/sJob.js
@@ -28,10 +28,10 @@ mp.events.add({
     },
     
     "sKeys-E" : (player) => {
-        if (!player.loggedIn || !player.canOpen.job) return;
+        //if (!player.loggedIn || !player.canOpen.job) return;
         //if (player.job.name && player.job.name !== player.canOpen.job) return player.notify(`~r~${i18n.get('basic', 'workingOnOtherJob', player.lang)}!`);
-        const job = getJobByName(player.canOpen.job);
-        job.pressedKeyOnMainShape(player);
+        //const job = getJobByName(player.canOpen.job);
+        //job.pressedKeyOnMainShape(player);
     },
 
 });
