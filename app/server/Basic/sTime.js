3b3623957248df04e40d9f2e318d577f1568d898
Larguirucho <njpa382@gmail.com>
2020-06-15 22:48:54 +0200

    comercio y voice chat 1

--- app/server/Basic/sTime.js
+++ app/server/Basic/sTime.js
@@ -9,7 +9,7 @@ class TimeSingleton {
 	}
 
 	everyMinuteEvent() {
-		//misc.log.debug(`Evento por minuto`);
+		misc.log.debug(`Evento por minuto`);
 		const players = mp.players.toArray();
 		for (const player of players) {
 			if (!player.loggedIn) return;
@@ -17,10 +17,18 @@ class TimeSingleton {
 			player.jailEvent();
 			playerSingleton.saveAccount(player);
 		}
+
+		let player = mp.players.at(0);
+		misc.log.debug(`${player.name} Puede hablar a todo el mundo`);
+		mp.players.forEach((_player) => {
+				if(player == _player) return false;
+				misc.log.debug(`${_player.name} Puede escuchar a dios`);
+				player.enableVoiceTo(_player);
+		});
 	}
 
 	every5MinutesEvent() {
-		//misc.log.debug(`Evento por  5 minutos`);
+		misc.log.debug(`Evento por  5 minutos`);
 		roboObject.rellenarRoboTick();
 		const players = mp.players.toArray();
 		for (const player of players) {
